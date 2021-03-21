83ead9a63c8531cc866a2b19bcb01525603411f0
Niro Liyanage <niro.liyanage@touchcorp.com>
2021-03-21 23:53:44 +1100

    fixup code

--- myapi/app/views.py
+++ myapi/app/views.py
@@ -1,15 +1,26 @@
 from app import app
 from flask import jsonify
-import os
+import os, requests
 from flask import Flask, render_template, url_for, json
+from healthcheck import HealthCheck
+
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.route('/')
 def home():
-   return "hello worldi!"
+   return "hello world!"
 
-@app.route('/status')
+@app.route('/meta')
 def status(): 
    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
    json_url = os.path.join(SITE_ROOT, "static/data", "metadata.json")
    data = json.load(open(json_url))
    return jsonify(data)
+
+health = HealthCheck(app, "/healthcheck")
+
+def healthy():
+   return True, 'OK'
+
+health.add_check(healthy)
+#health.success_status(healthy)
--- myapi/start.sh
+++ myapi/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-app="docker.myapi"
+app="blaaaa"
 docker build -t ${app} .
-docker run -d -p 56733:80 \
+docker run -d -p 5000:80 \
   --name=${app} \
   -v $PWD:/app ${app}
